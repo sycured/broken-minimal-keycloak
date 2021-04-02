cf516574573f0eaaef03d0d6c251ac30385504bb
sycured <60801403+sycured@users.noreply.github.com>
2021-04-02 18:01:20 -0500

    fix app: typo and missing flask configuration
    - typo about login endpoint: loogin to login
    - add Flask SECRET_KEY to be production compliant

--- app/app.py
+++ app/app.py
@@ -5,28 +5,34 @@ from flask import Flask, redirect
 from flask_jwt import JWT, jwt_required, current_identity
 from werkzeug.security import safe_str_cmp
 
-app = Flask(__name__)
-
 try:
     ISSUER_URL = os.environ["ISSUER_URL"]
     AUTH_URL = os.environ["AUTH_URL"]
     TOKEN_URL = os.environ["TOKEN_URL"]
     USERINFO_URL = os.environ["USERINFO_URL"]
     JWKS_URL = os.environ["JWKS_URL"]
+    SECRET_KEY = os.getenv(key='SECRET_KEY', default=None)
 except Exception as ex:
     raise
+
+app = Flask(__name__)
+app.secret_key = SECRET_KEY
+
 
 @app.route('/')
 def index():
     return 'Hello world!'
 
-@app.route("/loogin")
+
+@app.route("/login")
 def login():
     return redirect(AUTH_URL, code=302)
+
 
 @app.route("/auth/callback")
 def custom_callback():
     return "Logged in!"
+
 
 if __name__ == "__main__":
     app.run()
